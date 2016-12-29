2285bbf0a3e136425f310d692d9d06d9bcf02a11
michael.petrinolis <michael.petrinolis@outlook.com>
2016-12-29 14:55:35 +0200

    changed url

--- Clients/src/PhiloxenonDemoClientCredentialsFlow/Program.cs
+++ Clients/src/PhiloxenonDemoClientCredentialsFlow/Program.cs
@@ -43,7 +43,7 @@ namespace PhiloxenonDemoClientCredentialsFlow
                 var header = parts[0];
                 var claims = parts[1];
                 var claimValues = JObject.Parse(Encoding.UTF8.GetString(Base64Url.Decode(claims)));
-                tenant = claimValues.Value<string>("client_philoxenon_tenant");
+                tenant = claimValues.Value<string>("client_philoxenon_demo");
             }
             "Press enter to call service".ConsoleYellow();
             Console.ReadLine();
@@ -62,7 +62,7 @@ namespace PhiloxenonDemoClientCredentialsFlow
 
         static async Task CallServiceAsync(string token, string tenant)
         {
-            var baseAddress = $"http://localhost:5412/{tenant}/api/";
+            var baseAddress = $"http://localhost:5412/api/{tenant}/";
 
             var client = new HttpClient
             {
